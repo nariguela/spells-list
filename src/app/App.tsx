9dcb458d95550e1a1800449ac2208cfa454e97b3
Pedro <pedrodc3048@gmail.com>
2026-04-27 23:03:49 -0300

    feat: add fallback route and redirect configuration for app navigation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { AppShell } from "../components/layout/AppShell";
 import { AllSpellsPage } from "../features/spells/components/AllSpellsPage";
 import { SavedSpellsPage } from "../features/spells/components/SavedSpellsPage";
@@ -35,6 +35,7 @@ export default function App() {
           }
           path="/salvas"
         />
+        <Route element={<Navigate replace to="/" />} path="*" />
       </Routes>
     </AppShell>
   );
